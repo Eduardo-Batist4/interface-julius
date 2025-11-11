aa9c6e4b1eebcad25a0434b81df6ed6280868934
Eduardo Batista <carloswe3st@gmail.com>
2025-11-10 23:37:37 -0300

    feat: input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,14 +1,27 @@
-interface InputProps {
+import type { ChangeEvent, InputHTMLAttributes } from "react";
+
+interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "value"> {
   type: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   className?: string;
 }
 
-function Input({ type, className }: InputProps) {
+function Input({ type, value, className, onChange, ...res }: InputProps) {
   const defaultStyleInput = "w-full h-10 bg-white rounded-sm px-3";
 
   const combinedClassName = `${defaultStyleInput} ${className || ""}`;
 
-  return <input type={type} className={combinedClassName} />;
+  return (
+    <input
+      type={type}
+      className={combinedClassName}
+      value={value}
+      onChange={onChange}
+      {...res}
+    />
+  );
 }
 
 export default Input;
